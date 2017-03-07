6816e7e6e45c856029c4cea9db990945f165b4ef
amyhywong <amywong@Amys-MacBook-Pro-2.local>
2017-03-07 20:31:06 +0000

    added

--- Main.js
+++ Main.js
@@ -1,1 +1,20 @@
-<script type="text/javascript">
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge"> <!-- For intranet testing only, remove in production. -->
+  <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+  <title>Slide Show</title>
+</head>
+
+<body>
+  <div id="slideShowImages">
+    <img src="images/Gallery1.jpeg" alt="Slide 1" />
+    <img src="images/Gallery2.jpg" alt="Slide 2" />
+    <img src="images/Gallery3.jpg" alt="Slide 3" />    
+  </div>  
+  <button id="slideShowButton"></button> <!-- Optional button element. -->
+  <script src="slideShow.js"></script>
+</body>
+
+</html>
